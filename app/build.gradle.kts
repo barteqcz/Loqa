3518d666d4e694b2d2b9d255e5e32c52e23f6a15
Bartek <bartosz.brom06@gmail.com>
2026-08-27 01:35:07 +0200

    Bump version to 3.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.barteqcz.onqa"
         minSdk = 24
         targetSdk = 37
-        versionCode = 30201
-        versionName = "3.2.1"
+        versionCode = 30202
+        versionName = "3.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
