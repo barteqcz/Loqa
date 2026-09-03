439b8a3199c0b6d620095a2519b8d95e865e2c93
Bartek <118764028+barteqcz@users.noreply.github.com>
2026-09-03 16:19:48 +0200

    Delete app directory

--- app/src/main/java/com/barteqcz/onqa/data/model/StableLocation.kt
+++
@@ -1,9 +0,0 @@
-package com.barteqcz.onqa.data.model
-
-import androidx.compose.runtime.Immutable
-
-@Immutable
-data class StableLocation(
-    val latitude: Double,
-    val longitude: Double,
-)
--- app/src/main/java/com/barteqcz/onqa/ui/navigation/Routes.kt
+++
@@ -1,7 +0,0 @@
-package com.barteqcz.onqa.ui.navigation
-
-import kotlinx.serialization.Serializable
-
-@Serializable object RadioRoute
-@Serializable object SettingsRoute
-@Serializable object MapPickerRoute
